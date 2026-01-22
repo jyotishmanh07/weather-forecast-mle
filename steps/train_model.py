3ce62fe306caa01412c2a71d5bf82c27e3225ec3
jyotishman <jyotishmanhazarika@gmail.com>
2026-01-22 13:09:14 +0100

    modified train, evaluate and data cleaning

--- steps/train_model.py
+++ steps/train_model.py
@@ -1,4 +1,5 @@
 import mlflow
+import pandas as pd
 from zenml import step
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 from datasets import Dataset
@@ -12,21 +13,31 @@ def train_model(df: pd.DataFrame):
     model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
 
     # Convert to huggingface format
-    dataset = Dataset.from_pandas(df).map(lambda x: tokenizer(x['content'], truncation=True, padding='max_length'), batched=True)
-    
+    dataset = Dataset.from_pandas(df).map(
+        lambda x: tokenizer(
+            x['content'], 
+            truncation=True, 
+            padding='max_length', 
+            max_length=128
+        ), 
+        batched=True
+    )
     training_args = TrainingArguments(
         output_dir="./results",
         num_train_epochs=1,
-        per_device_train_batch_size=8,
-        report_to="mlflow"
+        per_device_train_batch_size=4, 
+        eval_strategy="no",     
+        report_to="mlflow",
+        use_cpu=True
     )
     
     trainer = Trainer(model=model, args=training_args, train_dataset=dataset)
     trainer.train()
-    
-    # Registering the model in MLflow model registry
+    pip_requirements = ["torch==2.10.0", "transformers", "datasets", "pandas"]
+
     mlflow.transformers.log_model(
         transformers_model={"model": model, "tokenizer": tokenizer},
-        artifact_path="news_model",
-        registered_model_name="NewsIntegrityModel"
+        name="news_model",
+        registered_model_name="NewsIntegrityModel",
+        pip_requirements=pip_requirements
     )
