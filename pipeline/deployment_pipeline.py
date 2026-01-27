f70bdfdfaf2747a3f740b3b862bb92607a4241b9
jyotishman <jyotishmanhazarika@gmail.com>
2026-01-27 10:53:43 +0100

    changes to deployment piplein

--- pipeline/deployment_pipeline.py
+++ pipeline/deployment_pipeline.py
@@ -1,6 +1,7 @@
 from zenml import pipeline, Model
 from steps.ingest_data import ingest_data
 from steps.clean_data import clean_data
+from steps.data_splitter import data_splitter  # New Import
 from steps.train_model import train_model
 from steps.evaluate_model import evaluate_model
 from steps.custom_deployer import custom_mlflow_deployer
@@ -12,6 +13,10 @@ from steps.custom_deployer import custom_mlflow_deployer
 def news_integrity_unified_pipeline():
     df = ingest_data()
     cleaned_df = clean_data(df)  
-    model_uri = train_model(cleaned_df) 
-    deployment_decision = evaluate_model(cleaned_df, model_uri)
+
+    train_df, test_df = data_splitter(cleaned_df)
+
+    model_uri = train_model(train_df) 
+    deployment_decision = evaluate_model(test_df, model_uri)
+    
     custom_mlflow_deployer(model_uri=model_uri, deploy_decision=deployment_decision)
